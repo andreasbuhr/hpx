2279bf54f9902df3db1b4b6c44d1af8c465de9e2
K-ballo <k@fusionfenix.com>
2013-09-30 11:00:05 -0500

    Draft when_any_swapped implementation

--- hpx/lcos/wait_any.hpp
+++ hpx/lcos/wait_any.hpp
@@ -12,6 +12,10 @@
 #include <hpx/hpx_fwd.hpp>
 #include <hpx/lcos/future.hpp>
 #include <hpx/lcos/wait_n.hpp>
+#include <hpx/lcos/local/packaged_task.hpp>
+#include <hpx/lcos/local/packaged_continuation.hpp>
+#include <hpx/runtime/threads/thread.hpp>
+#include <hpx/util/bind.hpp>
 #include <hpx/util/move.hpp>
 #include <hpx/util/tuple.hpp>
 #include <hpx/util/detail/pp_strip_parens.hpp>
@@ -19,12 +23,107 @@
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/enum.hpp>
 #include <boost/preprocessor/iterate.hpp>
+#include <boost/utility/swap.hpp>
 
 #include <vector>
 
 ///////////////////////////////////////////////////////////////////////////////
 namespace hpx
 {
+    namespace detail
+    {
+        ///////////////////////////////////////////////////////////////////////
+        template <typename T>
+        struct when_any_swapped : boost::enable_shared_from_this<when_any_swapped<T> >
+        {
+        private:
+            BOOST_MOVABLE_BUT_NOT_COPYABLE(when_any_swapped)
+
+            enum { index_error = -1 };
+
+            void on_future_ready(std::size_t idx, threads::thread_id_type id)
+            {
+                std::size_t index_not_initialized =
+                    static_cast<std::size_t>(index_error);
+                if (index_.compare_exchange_strong(index_not_initialized, idx))
+                {
+                    // reactivate waiting thread only if it's not us
+                    if (id != threads::get_self_id())
+                        threads::set_thread_state(id, threads::pending);
+                }
+            }
+
+        public:
+            typedef std::vector<lcos::future<T> > result_type;
+            typedef std::vector<lcos::future<T> > argument_type;
+
+            when_any_swapped(argument_type const& lazy_values)
+              : lazy_values_(lazy_values)
+              , index_(static_cast<std::size_t>(index_error))
+            {}
+
+            when_any_swapped(BOOST_RV_REF(argument_type) lazy_values)
+              : lazy_values_(boost::move(lazy_values))
+              , index_(static_cast<std::size_t>(index_error))
+            {}
+
+            when_any_swapped(BOOST_RV_REF(when_any_swapped) rhs)
+              : lazy_values_(boost::move(rhs.lazy_values_))
+              , index_(rhs.index_.load())
+            {
+                rhs.needed_count_ = 0;
+            }
+
+            when_any_swapped& operator=(BOOST_RV_REF(when_any_swapped) rhs)
+            {
+                if (this != &rhs) {
+                    lazy_values_ = boost::move(rhs.lazy_values_);
+
+                    index_ = rhs.index_;
+                    rhs.index_.store(static_cast<std::size_t>(index_error));
+                }
+                return *this;
+            }
+
+            result_type operator()()
+            {
+                using lcos::detail::get_future_data;
+
+                index_.store(static_cast<std::size_t>(index_error));
+
+                std::size_t size = lazy_values_.size();
+
+                // set callback functions to execute when future is ready
+                threads::thread_id_type id = threads::get_self_id();
+                for (std::size_t i = 0; i != size; ++i)
+                {
+                    lcos::detail::future_data_base<T>* current =
+                        get_future_data(lazy_values_[i]);
+
+                    current->set_on_completed(util::bind(
+                        &when_any_swapped::on_future_ready, this->shared_from_this(), i, id));
+                }
+
+                // If one of the futures is already set, our callback above has
+                // already been called, otherwise we suspend ourselves
+                if (index_.load() == static_cast<std::size_t>(index_error))
+                {
+                    // wait for any of the futures to return to become ready
+                    this_thread::suspend(threads::suspended);
+                }
+
+                // that should not happen
+                BOOST_ASSERT(index_.load() != static_cast<std::size_t>(index_error));
+
+                boost::swap(lazy_values_[index_], lazy_values_.back());
+                return boost::move(lazy_values_);
+            }
+
+            std::vector<lcos::future<T> > lazy_values_;
+            boost::atomic<std::size_t> index_;
+        };
+    }
+
     /// The function \a when_any is a non-deterministic choice operator. It
     /// OR-composes all future objects stored in the given vector and returns
     /// a new future object representing the first future from that list which
@@ -78,6 +177,63 @@ namespace hpx
         typedef HPX_STD_TUPLE<> result_type;
 
         return lcos::make_ready_future(result_type());
+    }
+
+    /// The function \a when_any_swapped is a non-deterministic choice operator. It
+    /// OR-composes all future objects stored in the given vector and returns
+    /// a new future object representing the first future from that list which
+    /// finishes execution.
+    ///
+    /// \return   The returned future holds the same list of futures as has
+    ///           been passed to when_any_swapped, except that the ready future
+    ///           has been swapped with the last one in the list.
+
+    template <typename R>
+    lcos::future<std::vector<lcos::future<R> > >
+    when_any_swapped(BOOST_RV_REF(HPX_UTIL_STRIP((
+        std::vector<lcos::future<R> >))) lazy_values,
+        error_code& ec = throws)
+    {
+        typedef std::vector<lcos::future<R> > result_type;
+
+        if (lazy_values.empty())
+            return lcos::make_ready_future(boost::move(lazy_values));
+
+        boost::shared_ptr<detail::when_any_swapped<R> > f =
+            boost::make_shared<detail::when_any_swapped<R> >(
+                boost::move(lazy_values));
+
+        lcos::local::futures_factory<result_type()> p(
+            util::bind(&detail::when_any_swapped<R>::operator(), f));
+
+        p.apply();
+        return p.get_future();
+    }
+
+    template <typename R>
+    lcos::future<std::vector<lcos::future<R> > > //-V659
+    when_any_swapped(std::vector<lcos::future<R> > const& lazy_values,
+        error_code& ec = throws)
+    {
+        typedef std::vector<lcos::future<R> > result_type;
+
+        result_type lazy_values_(lazy_values);
+        return when_any_swapped(boost::move(lazy_values_), ec);
+    }
+
+    template <typename Iterator>
+    lcos::future<std::vector<lcos::future<
+        typename lcos::detail::future_iterator_traits<Iterator>::traits_type::type
+    > > >
+    when_any_swapped(Iterator begin, Iterator end,
+        error_code& ec = throws)
+    {
+        typedef typename lcos::detail::future_iterator_traits<
+            Iterator>::traits_type::type value_type;
+        typedef std::vector<lcos::future<value_type> > result_type;
+
+        result_type lazy_values_(begin, end);
+        return when_any_swapped(boost::move(lazy_values_), ec);
     }
 
     /// The function \a wait_any is a non-deterministic choice operator. It
